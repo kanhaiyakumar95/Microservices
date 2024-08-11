c7f8f3301645f3ba64b092839ee0ee721e839bab
Kanhaiya Kumar <kanhaiyakumar95@gmail.com>
2024-08-11 18:13:55 +0530

    Fixed issue with reading cpnfig server property

--- department-service/src/main/java/net/javaguides/departmentservice/controller/MessageController.java
+++ department-service/src/main/java/net/javaguides/departmentservice/controller/MessageController.java
@@ -4,16 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RefreshScope
 @RestController
 public class MessageController {
 
-//    @Value("${spring.boot.message}")
-//    private String message;
+    @Value("${spring.boot.message}")
+    private String message;
 
     @GetMapping("message")
-    private String getMessage(@Value("${spring.boot.message}") String message){
+    public String message(){
         return message;
     }
 }
